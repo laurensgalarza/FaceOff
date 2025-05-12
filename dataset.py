732322e087dbfe6cedc94c6286c9c45961c40cb9
laurensgalarza <92804326+laurensgalarza@users.noreply.github.com>
2025-05-11 23:52:41 -0400

    Err handeling & documentation: dataset, inference

--- dataset.py
+++ dataset.py
@@ -5,24 +5,37 @@ import numpy as np
 import torch
 from facenet_pytorch import MTCNN
 
-import os
-import random
-from PIL import Image
-from torch.utils.data import Dataset
-from facenet_pytorch import MTCNN
 
-class SiameseDataset(Dataset):
+class SiameseDataset(Dataset):  
+    """
+    Custom dataset for training the neural network to compare faces with or without makeup.
+    Loads image pairs and determines whether the pair represents the same person (0.0) or different people (1.0)
+    """
     def __init__(self, makeup_dir, no_makeup_dir, mtcnn, image_size=112):
+        """
+        makeup_dir (string): Directory containing images of faces with makeup on.
+        no_makeup_dir (string): Directory containing images of faces without makeup.
+        mtcnn (MTCNN): Multi-task Cascaded Convolutional Networks algorithm. Detects faces and facial landmarks.
+        image_size (int, default=112): Size of the cropped and aligned face image.
+        """
         self.makeup_dir = makeup_dir
         self.no_makeup_dir = no_makeup_dir
         self.mtcnn = mtcnn
+        
+        # Get list of filenames (representing people) that exist in both directories
         self.people = list(set(os.listdir(makeup_dir)).intersection(os.listdir(no_makeup_dir)))
+        
+        if not self.people:
+            raise ValueError("Intersection is empty, no matching files present. Please make sure that image file names in makeup_dir are matching those in no_makeup_dir.")
         
         # self.makeup_files = os.listdir(makeup_dir)
         # self.no_makeup_files = os.listdir(no_makeup_dir)
         # self.common_files = list(set(self.makeup_files).intersection(set(self.no_makeup_files)))
 
     def __len__(self):
+        """
+        Returns the amount of people (matching files) found in the directories
+        """
         return len(self.people)
 
     # def load_face(self, image_path):
@@ -33,37 +46,62 @@ class SiameseDataset(Dataset):
     #     return face
 
     def __getitem__(self, idx):
-        should_get_same_class = random.randint(0, 1)
-
-        if should_get_same_class:
-            person = self.people[idx]
-            img0_path = os.path.join(self.makeup_dir, person)
-            img1_path = os.path.join(self.no_makeup_dir, person)
-            label = 0.0
-        else:
-            img0_name = random.choice(self.people)
-            img1_name = random.choice([f for f in self.people if f != img0_name])
-            img0_path = os.path.join(self.makeup_dir, img0_name)
-            img1_path = os.path.join(self.no_makeup_dir, img1_name)
-            label = 1.0
-
-        img0 = Image.open(img0_path).convert("RGB")
-        img1 = Image.open(img1_path).convert("RGB")
-
-        img0_tensor = self.mtcnn(img0)
-        img1_tensor = self.mtcnn(img1)
-
-        if img0_tensor is None or img1_tensor is None:
-            return self.__getitem__((idx + 1) % len(self))
+        """ 
+        Detects an image pair of the face with and without makeup, then aligns and crops to the image_size.
         
-        return img0_tensor, img1_tensor, torch.tensor([label], dtype=torch.float32)
-        # try:
-        #     img0 = self.load_face(img0_path)
-        #     img1 = self.load_face(img1_path)
-        # except Exception as e:
-        #     print(f"Skipping pair due to face detection failure: {e}")
-        #     return self.__getitem__((idx + 1) % len(self))
+        Returns:
+            img0_tensor (Tensor): First face tensor (image cropped and aligned with MTCNN)
+            img1_tensor (Tensor): Second face tensor
+            label (Tensor): Float tensor, 0.0. if same person and 1.0 if different people
+        """
+        # attempts are used for infinite recursion prevention
+        attempts_allowed = 5 
+        
+        for attempt in range(attempts_allowed):
+            try: 
+                # decide if returning pair of faces from the same person, or different people
+                should_get_same_class = random.randint(0, 1)
+
+                if should_get_same_class:
+                    person = self.people[idx]
+                    img0_path = os.path.join(self.makeup_dir, person)
+                    img1_path = os.path.join(self.no_makeup_dir, person)
+                    label = 0.0
+                else:
+                    img0_name = random.choice(self.people)
+                    img1_name = random.choice([f for f in self.people if f != img0_name])
+                    img0_path = os.path.join(self.makeup_dir, img0_name)
+                    img1_path = os.path.join(self.no_makeup_dir, img1_name)
+                    label = 1.0
+                
+                #Load images and convert to RGB
+                img0 = Image.open(img0_path).convert("RGB")
+                img1 = Image.open(img1_path).convert("RGB")
+                
+                #MTCNN aligns and crops images
+                img0_tensor = self.mtcnn(img0)
+                img1_tensor = self.mtcnn(img1)
+
+                if img0_tensor is not None and img1_tensor is not None:
+                    return img0_tensor, img1_tensor, torch.tensor([label], dtype=torch.float32)
+                
+                else:
+                    print(f"Face detection failed on attempt {attempt+1}. Retrying current input...")
+                
+            except Exception as e:
+                print(f"Failed to load image pair on attempt {attempt+1}: {e}")
+                
+            #Change index for next attempt
+            idx = (idx + 1) % len(self)
+            
+        raise RuntimeError("Failed to retrieve a valid image pairs after multiple attempts")
+            # try:
+            #     img0 = self.load_face(img0_path)
+            #     img1 = self.load_face(img1_path)
+            # except Exception as e:
+            #     print(f"Skipping pair due to face detection failure: {e}")
+            #     return self.__getitem__((idx + 1) % len(self))
 
 
 
-        # return img0, img1, label
+            # return img0, img1, label
--- inference.py
+++ inference.py
@@ -1,12 +1,22 @@
 import torch
 import torch.nn.functional as F
 from torchvision import transforms
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from facenet_pytorch import MTCNN
 import matplotlib.pyplot as plt
 from model import FaceOff 
+import os
 
 def show_result(img1, img2, distance, threshold):
+    """
+    Displays the input images side by side, along with the similarity score
+
+    Args:
+        img1 (PIL.Image): First face image
+        img2 (PIL.Image): Second face image
+        distance (float): Euclidean distance used to determine how similar the face tensors are
+        threshold (float): Cutoff used to determine whether the distance indicates the same or a different person
+    """
     fig, axes = plt.subplots(1, 2)
     axes[0].imshow(img1)
     axes[0].set_title("Image 1")
@@ -20,20 +30,63 @@ def show_result(img1, img2, distance, threshold):
     plt.show()
 
 def infer(image_path1, image_path2, model_path="./output/siamese_model.pth", threshold=1.0):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = FaceOff().to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device))
-    model.eval()
+    """
+    Runs inference on two face images using trained network and displays the result.
+    
+    Loads a trained model, detects faces in the provided images, computes their similarity, and shows the prediction.
 
+    Args:
+        image_path1 (string): Path to the first image file
+        image_path2 (string): Path to the second image file
+        model_path (string, optional): Path to the trained Siamese model file. Defaults to "./output/siamese_model.pth"
+        threshold (float, optional): Threshold for determining if the images are of the same person. Defaults to 1.0
+    """
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # Find model
+    model = FaceOff().to(device)
+    
+    #Return if model file not found
+    if not os.path.exists(model_path):
+        print(f"Model file not found at path: {model_path}")
+        return
+    
+    # Load model
+    try:
+        model.load_state_dict(torch.load(model_path, map_location=device))
+    
+    except Exception as e:
+        print(f"Failed to load model: {e}")
+        return
+    
+    model.eval()
+    
     mtcnn = MTCNN(image_size=112, margin=10)
 
     # Load and preprocess images
-    img1 = Image.open(image_path1).convert("RGB")
-    img2 = Image.open(image_path2).convert("RGB")
-
+    try:
+        img1 = Image.open(image_path1).convert("RGB")
+        img2 = Image.open(image_path2).convert("RGB")
+    # Errors:
+    # File not found 
+    except FileNotFoundError as e:
+        print(f"Image file not found: {e}")
+        return
+    # File found but is not a valid image
+    except UnidentifiedImageError as e:
+        print(f"Failed to open, not a valid image file: {e}")
+        return
+    #Other
+    except Exception as e:
+        print(f"An error occurred when opening images: {e}")
+        return        
+        
+    #Detect the faces    
     face1 = mtcnn(img1)
     face2 = mtcnn(img2)
-
+    
+    #Return if no faces detected
     if face1 is None or face2 is None:
         print("Face detection failed on one or both images.")
         return
@@ -41,9 +94,14 @@ def infer(image_path1, image_path2, model_path="./output/siamese_model.pth", thr
     face1 = face1.unsqueeze(0).to(device)
     face2 = face2.unsqueeze(0).to(device)
 
+    #Run inference
     with torch.no_grad():
-        output1, output2 = model(face1, face2)
-        distance = F.pairwise_distance(output1, output2).item()
+        try:
+            output1, output2 = model(face1, face2)
+            distance = F.pairwise_distance(output1, output2).item()
+        except Exception as e:
+            print(f"Inference failed: {e}")
+            return
 
     show_result(img1, img2, distance, threshold)
 
