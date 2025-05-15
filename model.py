b6fd1442e5bb98bdcee3ae73bf0035e541669c20
ChocoVan <jessica.chen580@myhunter.cuny.edu>
2025-05-14 23:47:41 -0400

    same model as siamese

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 class FaceOff(nn.Module):
     def __init__(self):
         super(FaceOff, self).__init__()
+        # Extracts features with CNN
         self.cnn1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=10),
             nn.ReLU(inplace=True),
@@ -18,7 +19,7 @@ class FaceOff(nn.Module):
             nn.Conv2d(128, 256, kernel_size=4),
             nn.ReLU(inplace=True)
         )
-
+        # Processes the 2D convolutional output to a 128-dimensional embedding vector
         self.fc1 = nn.Sequential(
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
@@ -26,23 +27,23 @@ class FaceOff(nn.Module):
         )
 
     def forward_once(self, x):
-        x = self.cnn1(x)
+        x = self.cnn1(x) 
         x = x.view(x.size()[0], -1)
         x = self.fc1(x)
         return x
 
-    def forward(self, input1, input2):
-        output1 = self.forward_once(input1)
-        output2 = self.forward_once(input2)
-        return output1, output2
+    # def forward(self, input1, input2):
+    #     output1 = self.forward_once(input1)
+    #     output2 = self.forward_once(input2)
+    #     return output1, output2
     
-class ContrastiveLoss(nn.Module):
-    def __init__(self, margin=2.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
+# class ContrastiveLoss(nn.Module):
+#     def __init__(self, margin=10.0):
+#         super(ContrastiveLoss, self).__init__()
+#         self.margin = margin
 
-    def forward(self, output1, output2, label):
-        euclidean_distance = F.pairwise_distance(output1, output2)
-        loss = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
-                            label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
-        return loss
+#     def forward(self, output1, output2, label):
+#         euclidean_distance = F.pairwise_distance(output1, output2)
+#         loss = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
+#                             label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+#         return loss
